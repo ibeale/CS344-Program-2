0a27f5e3f18c8a4a484102c57edbf19a24e99b2e
Isaac Beale <bealei@oregonstate.edu>
2020-05-03 19:08:07 -0700

    all done!

--- bealei.adventure.c
+++ bealei.adventure.c
@@ -1,3 +1,6 @@
+// Created by: Isaac Beale
+// for OSU CS344 SP'19
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -10,6 +13,9 @@
 
 #define STR_BUF 30
 
+
+// Similar struct to the one used in buildrooms, except the connections are just strings now instead of pointers
+// because it was too annoying to try and recreate the original struct.
 struct Room{
     char name[STR_BUF];
     char connections[6][STR_BUF];
@@ -17,6 +23,9 @@ struct Room{
 	int num_connections;
 };
 
+
+// Finds the most recent directory (not including . and ..) and returns the path to it.
+// Adapted from https://www.geeksforgeeks.org/c-program-list-files-sub-directories-directory/
 void getRecentDir(char path[STR_BUF]){
 	char buf[STR_BUF];
 	memset(buf,'\0', STR_BUF);
@@ -39,7 +48,6 @@ void getRecentDir(char path[STR_BUF]){
 			strcat(buf,de->d_name);
 			stat(buf, &file_info);
 			if(S_ISDIR(file_info.st_mode)){
-				// printf("%s: Last modified at: %d\n", de->d_name, file_info.st_mtime);
 				if(file_info.st_mtime > recent){
 					recent = file_info.st_mtime;
 					memset(path, '\0', STR_BUF);
@@ -53,6 +61,8 @@ void getRecentDir(char path[STR_BUF]){
 
 }
 
+
+// Helper function to remove the leading white character on a string because strtok is difficult.
 char* removeFirst(char string[STR_BUF]){
 	char new_str[STR_BUF];
 	memset(new_str, '\0', STR_BUF);
@@ -61,9 +71,12 @@ char* removeFirst(char string[STR_BUF]){
 		new_str[i-1] = string[i];
 		i++;
 	}
-	return new_str;
+	memset(string, '\0', STR_BUF);
+	strcpy(string, new_str);
 }
 
+
+// Iterate through each file inside of a directory
 void iterateFiles(char* path, struct Room* allrooms[7]){
 	char buf[STR_BUF];
 	DIR *dr = opendir(path);
@@ -80,8 +93,11 @@ void iterateFiles(char* path, struct Room* allrooms[7]){
 	de = readdir(dr);
 	char delim[] = ":\n";
 	int i = 0;
+
+	// Iterate through the files that are not named ".", "..", or "currentTime.txt"
 	while (de != NULL && i<7){
 		if(strcmp(de->d_name, ".") && strcmp(de->d_name, "..") && strcmp(de->d_name, "currentTime.txt")){
+			// Create a new room struct and initialize its variables.
 			allrooms[i] = (struct Room*) malloc(sizeof(struct Room));
 			int j = 0;
 			for(j; j < 6; j++){
@@ -97,6 +113,7 @@ void iterateFiles(char* path, struct Room* allrooms[7]){
 				return;
 			}
 			int k = 0;
+			// adapted from https://stackoverflow.com/questions/3501338/c-read-file-line-by-line
 			while ((read = getline(&line, &len, fp)) != -1){
 				char *ptr = strtok(line, delim);
 				
@@ -134,48 +151,115 @@ void iterateFiles(char* path, struct Room* allrooms[7]){
 	closedir(dr);
 }
 
+// Will use it's own thread to print the local time to a new file named currentTime.txt
+void* gettime(pthread_mutex_t* myMutex){
+	// Lock the resources so that the main process doesn't run
+	pthread_mutex_lock(myMutex);
+	char path[STR_BUF];
+	getRecentDir(path);
+	strcat(path,"/currentTime.txt");
+	FILE* fp = fopen(path, "w");
+	char buf[STR_BUF];
+	time_t cur_time;
+	cur_time = time(NULL);
+	strftime(buf, STR_BUF, "%-I:%M%P, %A, %B %d, %Y", localtime(&cur_time));
+	fputs(buf, fp);
+	fclose(fp);
+	// Release the resources to allow the main program to run.
+	pthread_mutex_unlock(myMutex);
+
+}
+
+
+// Prints the first line fo currentTime.txt
+void printFromTimeFile(){
+	char path[STR_BUF];
+	char content[STR_BUF];
+	getRecentDir(path);
+	strcat(path,"/currentTime.txt");
+	FILE* fp = fopen(path, "r");
+	fgets(content, STR_BUF, fp);
+	printf("\n%s\n", content);
+}
 
 
 void game(struct Room* allrooms[7]){
+	pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_lock(&myMutex);
+	pthread_t thread;
+	pthread_create( &thread, NULL, (void*) gettime, &myMutex);
 	int i = 0;
 	int j = 0;
 	int k;
-	int flag;
+	int flag = 0;
+	// path will hold the integer locations in the all rooms array of the visited
 	int path[100];
 	int path_len = 0;
 	size_t bufsize = STR_BUF;
 	size_t characters;
 	char* buffer;
 	buffer = (char*) malloc(STR_BUF);
+
+	//Find the location of the start room
 	for(i=0;i<7;i++){
 		if(!(strcmp(allrooms[i]->roomtype, "START_ROOM"))){
 			break;
 		}
 	}
 	struct Room* current = allrooms[i];
+	// While we're not in the end room, repeat
 	while(strcmp(current->roomtype, "END_ROOM")){
-		printf("CURRENT LOCATION: %s\n", current->name);
-		printf("POSSIBLE CONNECTIONS:");
-		j=0;
-		while(current->connections[j][0]){
-			printf(" %s", current->connections[j]);
-			j++;
+		// Flag = 2 if the time function is called. This is just to format the output correctly.
+		if(flag != 2){
+			printf("CURRENT LOCATION: %s\n", current->name);
+			printf("POSSIBLE CONNECTIONS:");
+			j=0;
+			while(current->connections[j][0] && j<6){
+				// If conditions are for output formatting.
+				if(j==0){
+					printf(" %s", current->connections[j]);
+					j++;
+				}
+				else{
+					printf(", %s", current->connections[j]);
+					j++;
+				}
+				
+			}
 		}
-		printf(".\nWHERE TO? >");
+		printf("\nWHERE TO? >");
 		memset(buffer, '\0', STR_BUF);
 		characters = getline(&buffer, &bufsize, stdin);
 		buffer[characters-1] = '\0';
-		flag = 1;
-		for(i=0;i<7;i++){
-			if(!(strcmp(allrooms[i]->name, buffer))){
-				path[path_len] = i;
-				path_len++;
-				current = allrooms[i];
-				flag = 0;
-				printf("\n");
+		flag = 0;
+		for(i=0;i<6;i++){
+			// check if the user input is connected to the current room
+			if(!(strcmp(current->connections[i], buffer))){
+				for(j=0;j<7;j++){
+					// Find the location in the array of the user inputted room
+					if(!(strcmp(allrooms[j]->name, buffer))){
+						path[path_len] = j;
+						path_len++;
+						current = allrooms[j];
+						flag = 1;
+						printf("\n");
+						break;
+					}
+				}
+			}
+			else if(!(strcmp("time", buffer))){
+				pthread_mutex_unlock(&myMutex);
+				pthread_join(thread, NULL);
+				pthread_mutex_lock(&myMutex);
+				pthread_create( &thread, NULL, (void*) gettime, &myMutex);
+				printFromTimeFile();
+				flag=2;
+				break;
+				
 			}
 		}
-		if(flag){
+		// If neither of the events above triggered, output the error message and try again.
+		if(flag == 0){
 			printf("\nHUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN.\n\n");
 		}
 	}
@@ -185,6 +269,10 @@ void game(struct Room* allrooms[7]){
 		printf("%s\n", allrooms[path[i]]->name);
 	}
 	free(buffer);
+	// Release the mutex so that we can join the dormant thread
+	pthread_mutex_unlock(&myMutex);
+	// Join dormant thread to free memory
+	pthread_join(thread, NULL);
 }
 
 void* run(){
@@ -202,25 +290,7 @@ void* run(){
 	return NULL;
 }
 
-void gettime(){
-	char path[STR_BUF];
-	getRecentDir(path);
-	strcat(path,"/currentTime.txt");
-	FILE* fp = fopen(path, "w");
-	char buf[STR_BUF];
-	time_t cur_time;
-	cur_time = time(NULL);
-	strftime(buf, STR_BUF, "%I:%M%P, %A, %B %d, %Y", localtime(&cur_time));
-	fputs(buf, fp);
-	fclose(fp);
-
-}
-
 int main(){
-	// int resultInt;
-	// int resultInt2;
-	// pthread_t threads[2];
-	// resultInt = pthread_create( &threads[0], NULL, run, NULL);
-	// resultInt = pthread_create( &threads[1], NULL, gettime, NULL);
-	gettime();
+	run();
+	exit(0);
 }
--- bealei.buildrooms.c
+++ bealei.buildrooms.c
@@ -1,3 +1,7 @@
+// Created by: Isaac Beale
+// for OSU CS344 SP'19
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -9,6 +13,7 @@
 #define NUM_ROOMS 10
 #define STR_BUF 30
 
+// Room struct that works like a linked list graph
 struct Room{
     char name[STR_BUF];
     struct Room* connections[6];
@@ -17,38 +22,44 @@ struct Room{
 	int next_open;
 };
 
-char* createDirectory(void){
+
+// This function creates the directory with the PID of the current process and the correct permissions
+char* createDirectory(char path[STR_BUF]){
     pid_t pid = getpid();
     int file_descriptor;
     char path_buffer[STR_BUF];
     memset(path_buffer, '\0', sizeof(path_buffer));
     sprintf(path_buffer, "bealei.rooms.%d/", pid);
     mkdir(path_buffer, 0755);
-	return path_buffer;
+	strcpy(path, path_buffer);
 }
 
 
+// createRooms takes in an array of allrooms and populates it with
+// malloc(). It first shuffles the names, and arbitrarily assigns the start, end and middle rooms
 int createRooms(struct Room* allrooms[7]){
     char roomnames[NUM_ROOMS][STR_BUF] = {
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-        "ten"
+        "Washington",
+        "Oregon",
+        "California",
+        "Colorado",
+        "Arizona",
+        "Utah",
+        "Montana",
+        "Idaho",
+        "New Mexico",
+        "Nevada"
     };
     shufflenames(roomnames);
     int i = 0;
 	int j = 0;
     for (i = 0; i < 7; i++){
         allrooms[i] = (struct Room*) malloc(sizeof(struct Room));
+		// Initialize each connection to NULL
 		for(j=0;j<7;j++){
 			allrooms[i]->connections[j] = NULL;
 		}
+		// Initialize other values
 		allrooms[i]->num_connections = 0;
 		allrooms[i]->next_open = 0;
         strcpy(allrooms[i]->name,roomnames[i]);
@@ -64,6 +75,8 @@ int createRooms(struct Room* allrooms[7]){
     }
 }
 
+
+// Shuffles an array of strings, used to ranomize the names of the rooms
 int shufflenames(char roomnames[NUM_ROOMS][STR_BUF]){
     int i = 0;
     char temp[STR_BUF];
@@ -76,19 +89,25 @@ int shufflenames(char roomnames[NUM_ROOMS][STR_BUF]){
     }
 }
 
+
+// Checks if two rooms are connected
 int areconnected(struct Room* r1, struct Room* r2){
 	int i =0;
 	for (i;i<6; i++){
 		int j = 0;
 		for (j; j<6; j++){
+			// If the first is connected to the second, and the second is connected to the first we return 1
 			if((r1->connections[i]->name == r2->name)&&(r2->connections[j]->name == r1->name)&&r1->connections[i]->name!=NULL){
 				return 1;
 			}
 		}
 	}
+	// if we get through the above loop, then they are not connected and return 0.
 	return 0;
 }
 
+
+// checks to see if we are able to add more connections to the room
 int connectionsarenotfull(struct Room* r){
 	if(r->num_connections < 6){
 		return 1;
@@ -98,7 +117,10 @@ int connectionsarenotfull(struct Room* r){
 	}
 }
 
+
+// Connect the two given rooms and increment their values accordingly.
 void connectrooms(struct Room* r1, struct Room* r2){
+	// Make sure the two rooms have capacity for connecting.
 	if (connectionsarenotfull(r1) && connectionsarenotfull(r2)){
 		r1->connections[r1->next_open] = r2;
 		r2->connections[r2->next_open] = r1;
@@ -109,7 +131,7 @@ void connectrooms(struct Room* r1, struct Room* r2){
 	}
 }
 
-
+// Another shuffle function, this time for a list of ints.
 void shufflearr(int arr[7]){
 	int i;
 	int temp;
@@ -123,14 +145,24 @@ void shufflearr(int arr[7]){
 
 }
 
+
+// Use the shufflearr function to randomize the connections
+// and for every room, connect to 3-6 other rooms.
 void connectallrooms(struct Room* allrooms[7]){
 	int num_connections;
 	int i =0;
 	int j = 0;
+	// this array will be shuffled to ensure that we don't try to connect two rooms twice
+	// this is done to uphold the rule for number of connections
 	int arr[7] = {0,1,2,3,4,5,6};
 	for(i=0;i<7;i++){
-		num_connections = (rand()%3) + 3;
+		// Generates a number [3,6]
+		num_connections = (rand()%4) + 3;
 		shufflearr(arr);
+		// This loop is here to make sure we are connecting a valid room, and not connecting a room to itself
+		// I subtract the number of connections the room has from the random number [3-6] so that 
+		// if there are, for example, 2 existing connections and we decide to add 3, the final number of connections
+		// will be 3 instead of 5.
 		for(j=num_connections-allrooms[i]->num_connections;j>0;j--){
 			if(i==arr[j]){
 				j--;
@@ -138,6 +170,7 @@ void connectallrooms(struct Room* allrooms[7]){
 					j=6;
 				}
 			}
+			// If the rooms aren't connected, then connect them.
 			if(!(areconnected(allrooms[i],allrooms[arr[j]]))){
 				connectrooms(allrooms[i],allrooms[arr[j]]);
 			}
@@ -146,7 +179,7 @@ void connectallrooms(struct Room* allrooms[7]){
 	}
 }
 
-
+// This takes in the array of room structs and produces the file to specification.
 void makefile(char path[STR_BUF], struct Room* allrooms[7]){
 	char name[STR_BUF];
 	char content[STR_BUF];
@@ -167,10 +200,6 @@ void makefile(char path[STR_BUF], struct Room* allrooms[7]){
 		fprintf(fp,"ROOM_TYPE: %s\n", allrooms[i]->roomtype);
 		fclose(fp);
 	}
-	//
-
-	// FILE *fp;
-	// fp = fopen("")
 }
 
 
@@ -183,7 +212,7 @@ int main()
     createRooms(allrooms);
 	connectallrooms(allrooms);
 	char path[STR_BUF];
-	strcpy(path,createDirectory());
+	createDirectory(path);
 	makefile(path, allrooms);
 	for (i = 0; i < 7; i++){
 		free(allrooms[i]);
